e0079de48d1bf2b79aff33fcdbfa8ce877b22f81
Nathália Figueredo <nathalia.figueredo@sou.inteli.edu.br>
2025-03-21 12:05:34 -0300

    Update welcome.js
    Adicionei game.device.os.desktop e uma condição para que quando mudasse o dispositivo de device de desktop para mibile a imagem do título fosse alterada

--- scenes/welcome.js
+++ scenes/welcome.js
@@ -12,10 +12,12 @@ export class WelcomeScene extends Phaser.Scene {
         //carrega as imagens usadas na cena
         this.load.image("paisagem", "./assets/paisagem.png");
         this.load.image("laboratorio", "./assets/laboratorio.png");
+        this.load.image("laboratorio2", "./assets/laboratorio2.png");
         this.load.image("grace", "./assets/grace.png");
         this.load.image("descricao", "./assets/descricao.png");
         this.load.image("objetivo", "./assets/objetivo.png");
         this.load.image("titulo", "./assets/missao_ciencia.png");
+        this.load.image("titulo1", "./assets/missao_ciencia1.png");
         this.load.image("play", "./assets/botao_play.png");
         this.load.image("controles", "./assets/controles.png");
         this.load.image("controlesImagem", "./assets/controles_imagem.png");
@@ -23,48 +25,51 @@ export class WelcomeScene extends Phaser.Scene {
     }
 
     create() {
-        //adiciona alguns elementos
-        this.add.image(this.larguraJogo/2, this.alturaJogo/2, "laboratorio");
-        this.add.image(this.larguraJogo/2, 120, "titulo").setScale(0.25);
-        this.add.image(this.larguraJogo/2, 270, "descricao").setScale(0.2);
-        this.add.image(this.larguraJogo/2, 400, "objetivo").setScale(0.2);
-        this.botaoJogar = this.add.image(this.larguraJogo/2 - 90, 500, "play").setScale(0.2).setInteractive();
-        this.botaoControles = this.add.image(this.larguraJogo/2 + 90, 500, "controles").setScale(0.2).setInteractive();
+        // Adiciona a imagem de fundo
+        this.add.image(this.larguraJogo / 2, this.alturaJogo / 2, 'laboratorio');
 
-        //add e configura botões
+        // Verifica se o dispositivo é desktop ou mobile
+        const tituloImage = this.sys.game.device.os.desktop ? 'titulo' : 'titulo1';
+        this.add.image(this.larguraJogo / 2, 120, tituloImage).setScale(0.25);
+
+        // Adiciona outros elementos
+        this.add.image(this.larguraJogo / 2, 270, "descricao").setScale(0.2);
+        this.add.image(this.larguraJogo / 2, 400, "objetivo").setScale(0.2);
+
+        this.botaoJogar = this.add.image(this.larguraJogo / 2 - 90, 500, "play").setScale(0.2).setInteractive();
+        this.botaoControles = this.add.image(this.larguraJogo / 2 + 90, 500, "controles").setScale(0.2).setInteractive();
+
+        // Configuração dos botões
         this.botaoJogar.on("pointerover", () => {
             this.input.setDefaultCursor("pointer");
         });
-        
+
         this.botaoJogar.on("pointerout", () => {
             this.input.setDefaultCursor("default");
         });
 
         this.botaoJogar.on("pointerdown", () => {
-            this.scene.start("MainScene")
-        })
-
-
+            this.scene.start("MainScene");
+        });
 
         this.botaoControles.on("pointerover", () => {
             this.input.setDefaultCursor("pointer");
         });
-        
+
         this.botaoControles.on("pointerout", () => {
             this.input.setDefaultCursor("default");
         });
 
-        //botão de controles para exibir os controles
+        // Botão de controles para exibir os controles
         this.botaoControles.on("pointerdown", () => {
             this.exibirControles();
-   
-        })
+        });
 
-        
+        this.cameras.main.clear();
     }
-    //exibe a imagem dos controles
-    exibirControles() {
 
+    // Exibe a imagem dos controles
+    exibirControles() {
         // Adiciona a imagem dos controles à tela
         this.controlesImage = this.add.image(this.larguraJogo / 2, this.alturaJogo / 2, "controlesImagem").setScale(0.3);
 
@@ -84,12 +89,9 @@ export class WelcomeScene extends Phaser.Scene {
             this.controlesImage.setVisible(false);
             this.botaoFechar.setVisible(false);
         });
-
     }
 
-
-
     update() {
-
+        // Atualiza a cena, se necessário
     }
 }
